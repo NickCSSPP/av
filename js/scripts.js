f98bb38ea8038ef9bb05734aa7c9b121962f1a76
Nick <nssurge@pointpark.edu>
2023-07-21 07:24:45 -0400

    Added JQuery, Jquery navigation and content changing

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('nav li a').on('click', function() {
+    const targetSection = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(targetSection).offset().top - 100
+    }, 1000);
+  });
+
+  $('input[name="fontType"]').change(function() {
+    const selectedFont = $(this).val();
+    $('body').css('font-family', selectedFont);
+  });
+
+  $('#fontColor').change(function() {
+    const selectedColor = $(this).val();
+    $('body').css('color', selectedColor);
+  });
+});
